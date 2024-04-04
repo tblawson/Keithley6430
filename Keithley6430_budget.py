06eb8efc5e8a0429780e70d9ff436e14641c7c3d
Tim Lawson <tim.lawson@callaghaninnovation.govt.nz>
2024-04-04 13:15:49 +1300

    Added Rs parameters lookup (from json file). Added default T and date (now). Added Rin lookup (from json file). Tidied-up output table.

--- Keithley6430_budget.py
+++ Keithley6430_budget.py
@@ -1,43 +1,87 @@
 # -*- coding: utf-8 -*-
 """
-Crude calculation of measured current uncertainty of a current source,
-using a sense resistor, Rs, and DVM connected across Rs.
+Updated calculation of measured current uncertainty of a current source, using a
+sense resistor, Rs, and DVM connected across Rs, accounting for dvm input impedance.
 """
 import GTC
+import json
+import datetime as dt
+import os
 
-#  V measurements
-V1 = GTC.ureal(1, 5e-7, 9, 'V1')
-V2 = GTC.ureal(1e-5, 5e-7, 9, 'V2')
-g1 = GTC.ureal(1.0000002, 4.2e-7, 121, 'g1')
-g2 = GTC.ureal(0.99995, 0.00057, 121, 'g2')
 
-#  DVM input
-Rdvm = GTC.ureal(1e12, 1.6e11, 10, 'Rdvm')
+'''
+# ------------------------
+Useful Classes / Functions
+--------------------------
+'''
+class UrealEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, GTC.lib.UncertainReal):
+            return {'__ureal__': True, 'val': obj.x, 'unc': obj.u, 'dof': obj.df}
+        return super().default(obj)
+
+
+def as_ureal(dct):
+    if '__ureal__' in dct:
+        return GTC.ureal(dct['val'], dct['unc'], dct['dof'])
+    else:
+        return dct
+
+
+# import Resistor info to RESISTORS dict
+with open('RESISTORS.json', 'r') as Resistors_fp:
+    RESISTORS = json.load(Resistors_fp, object_hook=as_ureal)
+
+# import dvm R-input info to DVM_RIN dict
+folder = r'G:\My Drive\TechProcDev\Keithley6430-src-meter_Light'
+sn = '452'  # input('\nEnter last 3 digits of 3458A serial number: ')
+dvm_Rin_filename = os.path.join(folder, f'HP3458A-{sn}_Rin.json')
+with open(f'{dvm_Rin_filename}', 'r') as Rin_fp:
+    DVM_RIN = json.load(Rin_fp, object_hook=as_ureal)
+
 
 #  Rs
-R0 = GTC.ureal(999.9969, 5.0e-4, 10, 'R0')  # (999.9969, 5.0e-4, 10, 'R0') (92622082837, 1.23e8, 8.2, 'R0')
-alpha = GTC.ureal(1.25e-6, 1.0e-7, 8, 'alpha')  # (1.25e-6, 1.0e-7, 8, 'alpha') (0, 1.25e-6, 8, 'alpha')
-T = GTC.ureal(21.2, 0.2, 10, 'T')
-T0 = GTC.ureal(21.0, 0.05, 10, 'T0')
-gamma = GTC.ureal(0, 1e-9, 3, 'gamma')  # (0, 1e-9, 3, 'gamma') (-1.14e-6, 2.1e-6, 8, 'gamma')
-V0 = GTC.ureal(1, 1e-6, 8, 'V0')  # (1, 1e-6, 8, 'V0') (200, 4.0e-4, 3, 'V0')
-tau = GTC.ureal(8.95e-10, 6.9e-11, 5, 'tau')  # (8.95e-10, 6.9e-11, 5, 'tau') (1e-6, 1e-6, 35, 'tau')
-t = GTC.ureal(45355.1, 0.1, 8, 't')
-t0 = GTC.ureal(45355, 0.1, 8, 't0')
+R_name = input(f'{RESISTORS.keys()}\nSelect sense resistor: ')
+
+R0 = RESISTORS[R_name]['R0']
+alpha = RESISTORS[R_name]['alpha']
+T0 = RESISTORS[R_name]['T0']
+gamma = RESISTORS[R_name]['gamma']
+V0 = RESISTORS[R_name]['V0']
+tau = RESISTORS[R_name]['tau']
+t0 = RESISTORS[R_name]['t0']
+t0_dt = dt.datetime.strptime(t0, '%d/%m/%Y %H:%M:%S')
+
+#  V measurements
+
+# Grab a temperature reading
+T_val = 20.5  # input('Rs temperature?: ')
+T = GTC.ureal(float(T_val), 0.05, 8, 'T')  # Resistor temp with type-B uncert.
+t = dt.datetime.now()
+t_str = t.strftime('%d/%m/%Y %H:%M:%S')
+delta_t = t - t0_dt  # datetime.timedelta object
+delta_t_days = GTC.ureal(delta_t.days + delta_t.seconds / 86400 + delta_t.microseconds / 8.64e10, 0.1, 8,
+                         'delta_t_days')
+
+V_off = GTC.ureal(6e-7, 3e-8, 9, 'V_off')
+V_on_args = input(f'Measured Vs as val, unc, dof: ').split(',')
+V_on = GTC.ureal(*map(float, V_on_args))
+
+#  DVM input
+key = f'{R_name}_V{V_on.x}'
+Rdvm = DVM_RIN[key]['Rin_approx']  # GTC.ureal(1e12, 1.6e11, 10, 'Rdvm')
 
 #  Sense Resistor
-Rs = R0*(1 + alpha*(T-T0) + gamma*(V1-V0) + tau*(t-t0))
+Rs = R0*(1 + alpha*(T - T0) + gamma*(V_on - V0) + tau*delta_t_days)
 
 #  Total parallel resistance (including input-Z of DVM)
 Rpar = Rs*Rdvm/(Rs+Rdvm)
 
 #  Current:
-I = (V1*g1 - V2*g2)/Rpar
+Imeas = (V_on - V_off) / Rpar
 
-params = {'V1': V1,
-          'V2': V2,
-          'g1': g1,
-          'g2': g2,
+params = {'V1': V_off,
+          'V2': V_on,
           'R0': R0,
           'alpha': alpha,
           'T': T,
@@ -45,13 +89,14 @@ params = {'V1': V1,
           'gamma': gamma,
           'V0': V0,
           'tau': tau,
-          't': t,
-          't0': t0,
+          'delta_t': delta_t_days,
           'Rdvm': Rdvm}
 
+print('Parameter\tUncert. \tSens. coef.\tU contrib (A)')
 for param in params:
-     sens = GTC.reporting.sensitivity(I, params[param])
-     u_cont = GTC.component(I, params[param])
-     print(f'{param}.....Sens. coef = {sens}. U contrib = {u_cont} A')
+    sens = GTC.reporting.sensitivity(Imeas, params[param])
+    u_cont = GTC.component(Imeas, params[param])
+    unc = params[param].x
+    print(f'{param:9}\t{unc:9.3g}\t{sens:11.3g}\t{u_cont:13.3g}')
 
-print(f'\nI = {I.x:.6e} +/- {I.u:.1e}, dof = {I.df:.1f}')
+print(f'\nI = {Imeas.x:.6e} +/- {Imeas.u:.1e}, dof = {Imeas.df:.1f}')
